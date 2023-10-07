75245f0b601433cca5bf5f69052b48fac63d2a6b
Alejandro Lopez <33030244+alejlopez139@users.noreply.github.com>
2023-10-07 07:24:05 +0000

    photo create works

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,46 @@
 class PhotosController < ApplicationController
 
   def index
-      render({:template => "photo_templates/index"})
+
+    matching_photos = Photo.all
+    @list_of_photos = matching_photos.order(:created_at => :desc)
+    render({:template => "photo_templates/index"})
+  end
+
+  def show
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.at(0)
+    
+    render({:template => "photo_templates/show"})
+  end
+
+  def baii
+
+    the_id = params.fetch("toast_id")
+    matching_photos = Photo.where({:id => the_id})
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    redirect_to("/photos")
+  end
+
+  def create
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_id")
+
+    a_new_photo = Photo.new
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    redirect_to("/photos/" + a_new_photo.id.to_s)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   get("/users/:path_username", :controller => "users", :action =>"show")
 
   get("/photos", {:controller => "photos", :action => "index"})
+  get("/photos/:path_id", {:controller => "photos", :action => "show"})
+  post("/insert_photo", {:controller => "photos", :action => "create"})
+  get("/delete_photo/:toast_id", {:controller => "photos", :action => "baii"})
 end
